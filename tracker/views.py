a85f76100f3e0e55d841df8689b01773d2482cad
Igor <pourovhr@gmail.com>
2025-12-28 22:07:38 +0500

    initial commit

---
+++ tracker/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+loh = "Лох"
+
+def home(request):
+    skills_with_stats = {}
+
+    for skill_id, skill in SKILLS.items():
+        done_count = sum(1 for goal in skill['goals'] if goal['done'])
+        total_count = len(skill['goals'])
+
+        skills_with_stats[skill_id] = {
+            **skill,
+            'done_count': done_count,
+            'total_count': total_count,
+        }
+
+    return render(request, "tracker/home.html", {
+        "skills": skills_with_stats,
+        "loh": loh
+    })
+
+SKILLS = {
+ 1: {
+ 'name': 'Python',
+ 'description': 'Master Python programming',
+ 'goals': [
+ {'id': 1, 'text': 'Изучить основы Python', 'done': True},
+ {'id': 2, 'text': 'Изучить основы словарей', 'done': False},
+ ]
+ },
+ 2: {
+ 'name': 'Django',
+ 'description': 'Master Django framework',
+ 'goals': [
+ {'id': 1, 'text': 'Изучить архитектуру Django', 'done': True},
+ {'id': 2, 'text': 'Изучить модуль views', 'done': True},
+ {'id': 3, 'text': 'Изучить модуль urls', 'done': False},
+ ]
+ }
+}
