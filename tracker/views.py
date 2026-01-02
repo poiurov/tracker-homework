73c78bb80a9c70c15e749961da7aebae0156bd0d
Igor <pourovhr@gmail.com>
2026-01-02 20:18:12 +0500

    Refactored few strings

--- tracker/views.py
+++ tracker/views.py
@@ -7,7 +7,7 @@ def home(request):
     skills_with_stats = {}
 
     for skill_id, skill in SKILLS.items():
-        done_count = sum(1 for goal in skill['goals'] if goal['done'])
+        done_count = sum(goal["done"] for goal in skill['goals'])
         total_count = len(skill['goals'])
 
         skills_with_stats[skill_id] = {
@@ -20,6 +20,7 @@ def home(request):
         "skills": skills_with_stats,
     })
 
+
 def skill_detail(request, skill_id):
     skill = SKILLS.get(skill_id)
 
@@ -30,22 +31,23 @@ def skill_detail(request, skill_id):
         "skill": skill,
     })
 
+
 SKILLS = {
- 1: {
- 'name': 'Python',
- 'description': 'Master Python programming',
- 'goals': [
- {'id': 1, 'text': 'Изучить основы Python', 'goal_description': 'Прочти книгу', 'done': True},
- {'id': 2, 'text': 'Изучить основы словарей', 'goal_description': 'Посмотри видео урок', 'done': False},
- ]
- },
- 2: {
- 'name': 'Django',
- 'description': 'Master Django framework',
- 'goals': [
- {'id': 1, 'text': 'Изучить архитектуру Django', 'goal_description': 'Посмотри видео урок', 'done': True},
- {'id': 2, 'text': 'Изучить модуль views', 'goal_description': 'Посмотри видео урок', 'done': True},
- {'id': 3, 'text': 'Изучить модуль urls', 'goal_description': 'Посмотри видео урок', 'done': False},
- ]
- }
+    1: {
+        'name': 'Python',
+        'description': 'Master Python programming',
+        'goals': [
+            {'id': 1, 'text': 'Изучить основы Python', 'goal_description': 'Прочти книгу', 'done': True},
+            {'id': 2, 'text': 'Изучить основы словарей', 'goal_description': 'Посмотри видео урок', 'done': False},
+        ]
+    },
+    2: {
+        'name': 'Django',
+        'description': 'Master Django framework',
+        'goals': [
+            {'id': 1, 'text': 'Изучить архитектуру Django', 'goal_description': 'Посмотри видео урок', 'done': True},
+            {'id': 2, 'text': 'Изучить модуль views', 'goal_description': 'Посмотри видео урок', 'done': True},
+            {'id': 3, 'text': 'Изучить модуль urls', 'goal_description': 'Посмотри видео урок', 'done': False},
+        ]
+    }
 }
